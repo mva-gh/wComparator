9ab2720d0e179e8b521072e34d8fc719e23c4e45
Wandalen <wandalen.me@gmail.com>
2018-11-03 14:30:36 +0200

    auto

--- sample/Sample.js
+++ sample/Sample.js
@@ -1,7 +1,9 @@
 
-let _ = require( 'wTools' );
+var _ = require( 'wcomparator' );
 
-/**/
+var map1 = { a : 0, e : { c : 2, d : 3 } };
+var map2 = { a : 0, e : { c : 2, d : 3 } };
+var map3 = { a : 0, e : { d : 4 } };
 
-...
-
+console.log( 'map1 ~ map2 :', _.entityIdentical( map1, map2 ) );
+console.log( 'map1 ~ map3 :', _.entityIdentical( map1, map3 ) );
